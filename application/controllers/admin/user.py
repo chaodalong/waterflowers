9c0ed24b51bebdb4768aaf9ae2a436c361198a2d
chaodalong <chadoalong@baofeng.com>
2017-08-19 19:19:38 +0800

    后台

--- application/controllers/admin/user.py
+++ application/controllers/admin/user.py
@@ -1,35 +1,44 @@
 # -*-coding: utf-8 -*-
-from flask import render_template, make_response, jsonify, request, flash
-from flask.views import View
+from flask import (render_template, request, redirect, url_for,
+session, flash)
+from .baseview import BaseView
 from application.models.UserModel import UserModel
+from application.utils.common import md5
 
-class User(View):
-    '''
-    用户类
-    '''
+class User(BaseView):
     methods = ['GET', 'POST']
 
-    def dispatch_request(self):
-        user = UserModel.query.filter(UserModel.id == 2).first()
-        user_obj = {"id":user.id, "name":user.name}
-        return jsonify(user_obj)
-
-class UserLogin(View):
-    methods = ['GET', 'POST']
+    def __init__(self, method_name=None):
+        self.method_name = method_name
 
     def dispatch_request(self):
+        return getattr(self, self.method_name)()
+
+    def login(self):
+        self.page_title = u'登录'
         if request.method == 'POST':
             username = request.form['username']
-            user = UserModel.query.filter(UserModel.name == username).first()
-            if user is None:
-                _user_save = UserModel(name=username)
-                UserModel.db_session.add(_user_save)
-                UserModel.db_session.commit()
-                flash(u"登陆失败！")
+            password = md5(request.form['password'])
+            if 'username' in session:
+                return redirect(url_for('bp_admin.water'))
             else:
-                UserModel.query.filter(UserModel.id > 2).update({UserModel.name:'tets2'})
-                UserModel.db_session.commit()
-                flash(u"登陆成功！")
-            return render_template('user/login.html')
+                user = UserModel.query.filter(UserModel.name == username).first()
+                print user
+                if user is not None and user.password == password:
+                    session['username'] = username
+                    return redirect(url_for('bp_admin.water'))
+                else:
+                    flash(u"用户名或密码错误")
+            return render_template('user/login.html', page_title=self.page_title)
         else:
-            return render_template('user/login.html')
+            if 'username' in session:
+                return redirect(url_for('bp_admin.water'))
+            else:
+                return render_template('user/login.html', page_title=self.page_title)
+
+    def logout(self):
+        self.page_title = '登出'
+
+        if 'username' in session:
+            del session['username']
+        return redirect(url_for('bp_admin.login'), page_title=self.page_title)
