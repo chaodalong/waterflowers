563ed5bd7e994c2a8952367aadc31ab916f6c7b8
chaodalong <chadoalong@baofeng.com>
2017-08-20 01:41:38 +0800

    语音提示

--- instance/config/sys.py
+++ instance/config/sys.py
@@ -9,6 +9,10 @@ ORDERS = {
     'begin_water': {
         'voice': 'static/voice/begin_water.mp3',
         'gpio_info': (22, 'out', 1) # 22引脚 输出 高电平
-    }
+    },
+    'stop_water': {
+        'voice': 'static/voice/stop_water.mp3',
+        'gpio_info': (22, 'out', 0) # 22引脚 输出 高电平
+    },
 }
 
