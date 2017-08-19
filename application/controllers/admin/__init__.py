0c3049dca80c816346b1f75cfc35d496019b8f76
chaodalong <chadoalong@baofeng.com>
2017-08-19 19:19:38 +0800

    后台

--- application/controllers/admin/__init__.py
+++ application/controllers/admin/__init__.py
@@ -1,14 +1,17 @@
 # -*-coding: utf-8-*-
 from flask.blueprints import Blueprint
-from .user import User, UserLogin
+from .user import User
+from .water import Water
 
 # blue print
 bp_admin = Blueprint('bp_admin', __package__)
 
-# user api
-bp_admin.add_url_rule('/user/', view_func=User.as_view('text'))
-
 # login
-bp_admin.add_url_rule('/login/', view_func=UserLogin.as_view('login'))
+bp_admin.add_url_rule('/login', view_func=User.as_view('login', method_name='login'))
+# login
+bp_admin.add_url_rule('/logout', view_func=User.as_view('logout', method_name='logout'))
+
+# dashboard
+bp_admin.add_url_rule('/water', view_func=Water.as_view('water'))
 
 __all__ = [bp_admin]
--- application/controllers/admin/water.py
+++ application/controllers/admin/water.py
@@ -1,14 +1,15 @@
 # -*-coding: utf-8 -*-
 from flask import render_template, redirect, url_for, session
-from flask.views import View
+from .baseview import BaseView
 
-class Dashboard(View):
+class Water(BaseView):
     methods = ['GET', 'POST']
+    page_title = u'浇花'
 
     def dispatch_request(self):
         if 'username' in session:
             username = session['username']
             data = {'username': username}
-            return render_template('dashboard/index.html', data=data)
+            return render_template('water/index.html', data=data, page_title=self.page_title)
         else:
-            return redirect(url_for('bp_front.login'))
+            return redirect(url_for('bp_admin.login'), page_title=self.page_title)
