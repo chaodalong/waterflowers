db700a74f69cf00c18f5207f6e234b8cb1ff011b
chaodalong <chadoalong@baofeng.com>
2017-08-22 13:21:07 +0800

    配置文件提取、mysql初始化方法重构

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask
 from .database.mysqldb import init_db
 
 
-'''
-设置日志handler
-'''
 def set_logger_handler(app):
+    """
+    设置日志handler
+    :param app:
+    :return:
+    """
     import logging
     from logging import Formatter
     from logging import FileHandler
@@ -18,17 +20,19 @@ def set_logger_handler(app):
     ))
     app.logger.addHandler(filehandler)
 
-'''
-创建应用
-'''
+
 def create_app():
+    """
+    创建应用
+    :return: app instance
+    """
     app = Flask(__name__.split('.')[0], instance_relative_config=True)
 
     app.config.from_pyfile('config/sys.py')
 
     set_logger_handler(app)
 
-    init_db()
+    init_db(app)
 
     from application.controllers.admin import bp_admin
     app.register_blueprint(bp_admin, url_prefix='/admin')
--- instance/config/sys.py
+++ instance/config/sys.py
@@ -2,13 +2,13 @@ HOST="127.0.0.1"
 
 DEBUG = True
 
-SECRET_KEY = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
+SECRET_KEY = "A0Zr98j/3yX R~XLL!jmN]LWX/,?RT"
 
 # 指令
 ORDERS = {
     'begin_water': {
         'voice': 'static/voice/begin_water.mp3',
-        'gpio_info': (22, 'out', 1) # 22引脚 输出 高电平
+        'gpio_info': (22, 'out', 1)  # 22引脚 输出 高电平
     },
     'stop_water': {
         'voice': 'static/voice/stop_water.mp3',
@@ -19,4 +19,6 @@ ORDERS = {
         'gpio_info': (22, 'out', 0)
     },
 }
+
+DATABASE = 'mysql+pymysql://root:123456@127.0.0.1:3306/waterflowers?charset=utf8'
 
